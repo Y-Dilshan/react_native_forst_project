6ac3cc61845238992bb0c6ec7d9134198ca8be75
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-09-11 14:03:06 +0530

    created native project

---
+++ api/models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'please add the name'],
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        match: [/\S+@\S+\.\S+/, 'is invalid']
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a password'],
+        minlength: [6, 'Password must be at least 6 characters']
+    }
+}, {timestamps: true});
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
